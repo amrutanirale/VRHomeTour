1f3d88d94e9a04248fe7a678437f9413b2f0664b
Amruta <niraleamruta@gmail.com>
2018-12-18 22:39:43 +0530

    no message

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+    // Update is called once per frame...
     void Update()
     {
         ToggleCheck();
